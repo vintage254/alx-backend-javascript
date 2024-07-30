69fb92653b3339d754fcb6e1e8dc4854985f696e
vintage254 <derricknjuguna414@gmail.com>
2024-07-30 03:54:04 -0700

    fix

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,7 @@
-export default function getListStudentIds(array){
-  if (!Array.isArray(array)){
+export default function getListStudentIds(array) {
+  if (!Array.isArray(array)) {
     return [];
   }
-
-  return array.map((a) => a.id)
-
+  return array.map((a) => a.id);
 }
+
