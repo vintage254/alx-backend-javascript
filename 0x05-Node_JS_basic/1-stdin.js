36977e7b0b4d9fc066cc2bbbb7fa3154216efd66
vintage254 <derricknjuguna414@gmail.com>
2024-09-17 11:04:23 +0000

    task1 fix

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,9 +2,9 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('readable', () => {
 	const input = process.stdin.read();
-	if(input !== null) console.log(`Your name is: ${input}`);
+	if(input !== null) process.stdout.write(`Your name is: ${input}`);
 });
 
 process.stdin.on('end', () => {
-	console.log('This important software is now closing');
+	process.stdout.write('This important software is now closing\n');
 });
